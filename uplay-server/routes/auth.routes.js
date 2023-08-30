594801ef55552bca8be2e001c807d0c41c208210
“Emeka <ngwe7236@gmail.com>
2023-08-30 18:32:59 +0200

    d

--- uplay-server/routes/auth.routes.js
+++ uplay-server/routes/auth.routes.js
@@ -63,7 +63,7 @@ router.post("/signup", (req, res, next) => {
     })
 
     .catch((error) => {
-      console.log("===showRegisterError==>", error);
+      console.log("===showRegisterError innit==>", error);
     });
 });
 
